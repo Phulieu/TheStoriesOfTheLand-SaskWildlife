d989b2ed66fa94cf046e8a467001bec132f84bab
Nayana Harish <harish0878@saskpolytech.ca>
2023-05-06 11:06:06 -0600

    removed update and delete

--- StoriesOfLandAPI/server.js
+++ StoriesOfLandAPI/server.js
@@ -3,10 +3,12 @@ const db = require('./db/connect');
 
 const backend = express();
 
-//backend.use(express.json());
+backend.use(express.json());
 
 // listen for connections (on port 3001)
 backend.listen(3001, function(){
     // send message that we are listening
     console.log("server started");
 });
+
+
