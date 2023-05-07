50a9e0e1e8cac2cc49a81e7543e37cec879adc82
Nayana Harish <harish0878@saskpolytech.ca>
2023-05-06 23:41:27 -0600

    added images and audio

--- StoriesOfLandAPI/server.js
+++ StoriesOfLandAPI/server.js
@@ -6,17 +6,14 @@ const backend = express();
 backend.use(express.json());
 
 const router = require('./routes/routes');
-<<<<<<< HEAD
+
 backend.use('/api',router);
 
 //index route
 backend.get('/',function(request,response) {
     response.redirect('/api/plant');
 })
-// listen for connections (on port 3001)
-backend.listen(3001, function(){
 
-=======
 
 backend.use('/api', router);
 
@@ -27,7 +24,7 @@ backend.get('/', function (request, response) {
 
 // listen for connections (on port 3001)
 backend.listen(3001, function () {
->>>>>>> 2385a0cd129ee4a56704c155d372d7f07d26e9e2
+
     // send message that we are listening
     console.log("server started");
 });
