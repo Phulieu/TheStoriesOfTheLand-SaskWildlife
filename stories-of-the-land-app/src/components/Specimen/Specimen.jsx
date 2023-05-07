4ac50398d33cecff28db5724b28d11063fd6f3e9
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-05-06 22:10:53 -0600

    Add styled components to specimen.jsx

--- stories-of-the-land-app/src/components/Specimen/Specimen.jsx
+++ stories-of-the-land-app/src/components/Specimen/Specimen.jsx
@@ -1,8 +1,34 @@
+import styled from "styled-components";
+
+const SpecimenContainer = styled.div.attrs({
+    className: 'card',
+})``;
+
+const SpecimenBody = styled.div.attrs({
+    className: 'card-body'
+})``;
+
+const SpecimenImg = styled.img.attrs({
+    className: 'card-img-top'
+})``;
+
+const SpecimenTitle = styled.h5.attrs({
+    className: 'card-title'
+})``;
+
+const EditButton = styled.button.attrs({
+    className: 'btn btn-primary'
+})``;
+
 function Specimen(){
     return(
-        <div>
-            Specimen Test 1
-        </div>
+        <SpecimenContainer>
+            <SpecimenImg/>
+            <SpecimenBody>
+                <SpecimenTitle>Specimen Test</SpecimenTitle>
+                <EditButton>Edit</EditButton>
+            </SpecimenBody>
+        </SpecimenContainer>
     );
 }
 
