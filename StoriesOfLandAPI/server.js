247828622575746d139bb6c40c85d7cc7ded6497
Nayana Harish <harish0878@saskpolytech.ca>
2023-05-06 19:52:31 -0600

    made changes to api ,now it's working

--- StoriesOfLandAPI/server.js
+++ StoriesOfLandAPI/server.js
@@ -5,8 +5,16 @@ const backend = express();
 
 backend.use(express.json());
 
+const router = require('./routes/routes');
+backend.use('/api',router);
+
+//index route
+backend.get('/',function(request,response) {
+    response.redirect('/api/plant');
+})
 // listen for connections (on port 3001)
 backend.listen(3001, function(){
+
     // send message that we are listening
     console.log("server started");
 });
