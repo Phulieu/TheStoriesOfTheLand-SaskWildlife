2facbf566c51afabcb869a22b41464a69782ecdf
Jason Jacob <8477@saskpolytech.ca>
2023-05-07 19:51:10 -0600

    React api calls

---
+++ stories-of-the-land-app/src/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL : 'http://localhost:3001/api'
+});
+
+//get all
+const getAllPlants = async () => {
+    return api.get('/plant');
+};
+
+// get by id
+const getPlantById= async (id) => {
+    return api.get(`/plant/${id}`);
+};
+
+// create
+const createPlant = async (payload) => {
+    return api.post('/plant',payload);
+};
+
+
+const apiCalls = {
+    getAllPlants,
+    getPlantById,
+    createPlant
+}
+
+export default apiCalls;
