f8aa62e062ae01a1a620b3a127ef22de18b26e5a
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-05-08 18:28:50 -0600

    refactor SpecimenList component to use boostrap

--- stories-of-the-land-app/src/views/SpecimenList.jsx
+++ stories-of-the-land-app/src/views/SpecimenList.jsx
@@ -1,90 +1,79 @@
 import { useEffect, useState } from "react";
 import { Specimen } from "../components";
-import styled from "styled-components";
 import apiCalls from "../api";
 
-const SpecimenListContainer = styled.div`
-    display: grid;
-    min-height: 100vh;
-    grid-template-columns: repeat(auto-fit, minmax(300px,1fr));
-    gap: 0 16px;
-    align-items: center;
-`;
-
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 999;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const ModalContent = styled.div`
-  position: relative;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 10px;
-  max-width: 80%;
-  max-height: 80%;
-  width: 600px;
-  height: 550px;
-  overflow-y: auto;
-`;
-const CloseButton = styled.span`
-  position: absolute;
-  top: -2px;
-  right: 12px;
-  font-size: 30px;
-  cursor: pointer
-`;
 const SpecimenList = () => {
-    const [specimens, setSpecimens] = useState();
-    const [selectedSpecimen,setSelectedSpecimen] = useState(null);
+  const [specimens, setSpecimens] = useState([]);
+  const [selectedSpecimen, setSelectedSpecimen] = useState(null);
 
-    useEffect(()=>{
-        apiCalls.getAllPlants().then(
-            (res)=>{
-                setSpecimens(res.data.data);
-            }
-        ).catch(console.error);
-    }, []);
-    
-    
-    const handleSpecimenClick = (specimen) => {
-        setSelectedSpecimen(specimen);
-    }
-    const handleCloseModal = () => {
-      setSelectedSpecimen(null);
-    }
+  useEffect(() => {
+    apiCalls
+      .getAllPlants()
+      .then((res) => {
+        setSpecimens(res.data.data);
+      })
+      .catch(console.error);
+  }, []);
 
-    return (
-      
-        <SpecimenListContainer>
-            {
-                  specimens && specimens.map(specimen =>
-                    <div onClick={() => handleSpecimenClick(specimen)}>
-                        <Specimen
-                        key={specimen._id}
-                        title={specimen.plantName}
-                        url={'http://localhost:3001' + specimen.image}/>
-                    </div>
-                    )
-            }
-            {selectedSpecimen && (
-              <ModalContainer>
-                <ModalContent>
-                  <CloseButton onClick={handleCloseModal}>&times;</CloseButton>
-                  <h2>{selectedSpecimen.plantName}</h2>
-                  <img src={'http://localhost:3001' + selectedSpecimen.image} alt="the specimen" />
-                  <p>Story of the land: {selectedSpecimen.story}</p>
-                </ModalContent>
-              </ModalContainer>
-            )}
-        </SpecimenListContainer>
-    );
+  const handleSpecimenClick = (specimen) => {
+    setSelectedSpecimen(specimen);
+  };
+  const handleCloseModal = () => {
+    setSelectedSpecimen(null);
+  };
+
+  return (
+    <div className="container py-5">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {specimens.map((specimen) => (
+          <div
+            key={specimen._id}
+            className="col"
+            onClick={() => handleSpecimenClick(specimen)}
+          >
+            <Specimen
+              title={specimen.plantName}
+              url={"http://localhost:3001" + specimen.image}
+            />
+          </div>
+        ))}
+      </div>
+      {selectedSpecimen && (
+        <div
+          className="modal fade show"
+          style={{ display: "block" }}
+          tabIndex="-1"
+          role="dialog"
+          onClick={handleCloseModal}
+        >
+          <div className="modal-dialog modal-dialog-centered modal-lg">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">{selectedSpecimen.plantName}</h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={handleCloseModal}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <img
+                  src={"http://localhost:3001" + selectedSpecimen.image}
+                  alt="the specimen"
+                  className="img-fluid mb-3"
+                />
+                <p>Story of the land: {selectedSpecimen.story}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
-export default SpecimenList; 
+
+export default SpecimenList;
