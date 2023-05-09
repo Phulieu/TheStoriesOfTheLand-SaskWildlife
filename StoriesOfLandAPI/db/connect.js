253634d95ab216929f4506a130007f53cf7c9c19
Nayana Harish <harish0878@saskpolytech.ca>
2023-05-09 15:14:26 -0600

    added internal comments to the api

--- StoriesOfLandAPI/db/connect.js
+++ StoriesOfLandAPI/db/connect.js
@@ -1,22 +1,47 @@
+
+/**
+ *  This line imports the Mongoose library.
+ *  which interacts with MongoDB databases.
+ */
 const mongoose = require('mongoose');
 
+/**
+ * set 'strictQuery' option to true for the Mongoose library.
+ * so Mongoose will throw an error if a query contains unknown keys.
+ */
 mongoose.set('strictQuery', true);
+
+
+/**
+ *constant stores credentials and  the URI needed to connect to the MongoDB database using the Mongoose library.
+ */
 
 const username = 'nayana-harish';
 const password = 'Adwaith-2017';
 const dbname = "story_of_land_app"
 const uri = `mongodb+srv://${username}:${password}@cluster0.qsvvngd.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
-mongoose.connect(uri).then(
+
+
+//connects to the MongoDB database using the Mongoose library and the URI stored in the 'uri' constant.
+mongoose.connect(uri).
+
+//on successful connection then block will be executed.
+then(
+
     (con)=>{
         console.log("Connected to MongoDB");
     }
-).catch(
+).
+//on error catch block will be executed.
+catch(
     (error)=>{
         console.log("Got error: " + error);
     }
 );
 
+//creates a reference to the default Mongoose connection object.
 const db = mongoose.connection;
 
+//exports the 'db' constant so that it can be used by other modules in the application
 module.exports = db;
--- StoriesOfLandAPI/db/models/Plants.js
+++ StoriesOfLandAPI/db/models/Plants.js
@@ -1,4 +1,12 @@
+/**
+ *  This line imports the Mongoose library.
+ *  which interacts with MongoDB databases.
+ */
 const mongoose = require('mongoose');
+
+/**
+ * define a new Mongoose schema for the 'plant' collection in the MongoDB database.
+ */
 
 const plantSchema = new mongoose.Schema({
     plantName: {type: String, required: true},
@@ -7,6 +15,10 @@ const plantSchema = new mongoose.Schema({
     audio: {type: String, required: false}
 });
 
+/**
+ * creates a new Mongoose model for the 'plants' collection in the MongoDB database.
+ */
 const Plant = mongoose.model("Plants", plantSchema);
 
+//exports the 'Plant' constant so that it can be used by other modules in the application
 module.exports = Plant;
