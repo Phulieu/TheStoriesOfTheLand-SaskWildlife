39183f02294f6329ef8a0eaf68950a15315982a8
Phulieu <parkerlieu1999@gmail.com>
2023-05-15 15:45:36 -0600

    wip login view

---
+++ stories-of-the-land-app/src/views/LogInView.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { UserInput } from "../components";
+import { useNavigate } from "react-router-dom";
+import styles from "./LogInView.module.css";
+
+function LogInView() {
+
+    
+    const [username,setUsername] = useState();
+    const [password,setPassword] = useState();
+
+
+    const navigate = useNavigate();
+
+    const loginUser = async (username, password) => {
+        // apiCalls.login({username,password}).then(() => {
+        //     navigate('/plant/list')
+        // }).catch((err) => {
+        //     console.log(err);
+        // })
+    }
+    return (
+        <div className={`${styles.loginBackground} w-100`}>
+            <div className={styles.loginContainer}>
+            <h2 className={styles.loginTitle}>User Login</h2>
+                    <div className={styles.loginForm}>
+                        <label>Username:</label>
+                        <UserInput
+                            setValue = {setUsername}
+                            initialValue =""
+                        />
+                        <label>Password:</label>
+                        <UserInput
+                            setValue = {setPassword}
+                            initialValue =""
+                            type = "password"
+                        />
+                        <button className={styles.loginButton} onClick = {() => {
+                            loginUser(username,password);
+                        }}>Login</button>
+                    </div>
+            </div>
+        </div>
+      );
+}
+
+export default LogInView;
