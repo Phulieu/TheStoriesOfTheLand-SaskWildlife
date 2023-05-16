c5127c72fc8435c4874d86e639e195e25644cc85
Jason Jacob <8477@saskpolytech.ca>
2023-05-16 13:17:27 -0600

    Error handling

--- stories-of-the-land-app/src/app/index.jsx
+++ stories-of-the-land-app/src/app/index.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { LogInView, SpecimenList } from "../views";
-import { QRGenerate, SpecimenList } from "../views";
+import { QRGenerate, LogInView, SpecimenList } from "../views";
 import SpecimenView from "../views/SpecimenView";
-
-
 
 /**
  * MAIN react 
