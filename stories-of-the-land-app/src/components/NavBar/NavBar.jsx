1ba4e4e2cea8b36c41de0368b069dfbbb4311b4f
Parker Lieu <lieu8336@saskpolytech.ca>
2023-05-18 18:44:55 -0600

    Adding navigation and logout

--- stories-of-the-land-app/src/components/NavBar/NavBar.jsx
+++ stories-of-the-land-app/src/components/NavBar/NavBar.jsx
@@ -1,20 +1,52 @@
-import Links from "../Links";
-import Logo from "./Logo";
-import styles from "./NavBar.module.css";
+import React, { useState } from 'react';
+import Links from '../Links';
+import Logo from './Logo';
+import styles from './NavBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import apiCalls from '../../api';
+import { useNavigate } from 'react-router-dom';
 
 /**
  * Navigation bar component for the admin
  * @returns navigation bar
  */
 const NavBar = () => {
-    return(
-        <div className={styles.container}>
-            <nav className={`navbar navbar-expand-lg navbar-dark ${styles.navbar}`}>
-                <Logo/>
-                <Links/>
-            </nav>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    apiCalls
+      .logout()
+      .then(() => {
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <nav className={`navbar navbar-expand-lg navbar-dark ${styles.navbar}`}>
+        <Logo />
+        <Links />
+        <div className={`${styles.menu} ${isMenuOpen ? styles.open : ''}`}>
+          <ul className={styles.menuList}>
+            <li>Feedback</li>
+            <li onClick={handleLogout}>Log out</li>
+          </ul>
         </div>
-    );
+        <button className={styles.menuButton} onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
--- stories-of-the-land-app/src/views/LogInView.jsx
+++ stories-of-the-land-app/src/views/LogInView.jsx
@@ -5,46 +5,59 @@ import styles from "./LogInView.module.css";
 import apiCalls from "../api";
 
 function LogInView() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    
-    const [username,setUsername] = useState();
-    const [password,setPassword] = useState();
+  const navigate = useNavigate();
 
+  const loginUser = async (username, password) => {
+    apiCalls
+      .login({ username, password })
+      .then(() => {
+        navigate("/plant/list");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const navigate = useNavigate();
-
-    const loginUser = async (username, password) => {
-        apiCalls.login({username,password}).then(() => {
-            navigate('/plant/list')
-        }).catch((err) => {
-            console.log(err);
-        })
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      loginUser(username, password);
     }
-    return (
-        <div className={`${styles.loginBackground} w-100`}>
-            <div className={styles.loginContainer}>
-            <h2 className={styles.loginTitle}>User Login</h2>
-                    <div className={styles.loginForm}>
-                        <label className={styles.loginLabel}>Username:</label>
-                        <UserInput
-                            setValue = {setUsername}
-                            initialValue =""
-                            className ={styles.loginInput}
-                        />
-                        <label className={styles.loginLabel}>Password:</label>
-                        <UserInput
-                            setValue = {setPassword}
-                            initialValue =""
-                            type = "password"
-                            className ={styles.loginInput}
-                        />
-                        <button className={styles.loginButton} onClick = {() => {
-                            loginUser(username,password);
-                        }}>Login</button>
-                    </div>
-            </div>
+  };
+
+  return (
+    <div className={`${styles.loginBackground} w-100`}>
+      <div className={styles.loginContainer}>
+        <h2 className={styles.loginTitle}>User Login</h2>
+        <div className={styles.loginForm}>
+          <label className={styles.loginLabel}>Username:</label>
+          <UserInput
+            setValue={setUsername}
+            initialValue=""
+            className={styles.loginInput}
+            onKeyPress={handleKeyPress}
+          />
+          <label className={styles.loginLabel}>Password:</label>
+          <UserInput
+            setValue={setPassword}
+            initialValue=""
+            type="password"
+            className={styles.loginInput}
+          />
+          <button
+            className={styles.loginButton}
+            onClick={() => {
+              loginUser(username, password);
+            }}
+          >
+            Login
+          </button>
         </div>
-      );
+      </div>
+    </div>
+  );
 }
 
 export default LogInView;
