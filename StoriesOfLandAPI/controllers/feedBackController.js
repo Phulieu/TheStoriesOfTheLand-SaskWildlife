2ff6ba5efa437bddd3225be6efedfd21c99cfe60
Nayana Harish <harish0878@saskpolytech.ca>
2023-06-02 06:41:32 -0600

    backend-admin-feedback-completed

--- StoriesOfLandAPI/controllers/feedBackController.js
+++ StoriesOfLandAPI/controllers/feedBackController.js
@@ -22,9 +22,49 @@ const createFeedback = async (req, res) => {
       return res.status(400).json({ success: false, error: error.message });
     }
   }
+
+
+
+  const getAllFeedback = async (req, res) => {
+
+    //call find method to return all plants
+    Feedback.find().
+        //on success call lambda function.
+        then((feedback) => {
+
+            //check each plant object's length to make sure the content is there.
+            if (!feedback.length) {
+                //if length zero means ,no data response return as json and error message 404
+                return res.status(404).json({ success: false, error: "No Feedback found." });
+            }
+            //if length is greater than zero means data is present and it displays data with status code 200.
+            return res.status(200).json({ success: true, data: feedback });
+        }).
+        //on error catch block will be executed with anonymous lambda function and returns status code 400 with error message.
+        catch((err) => {
+            return res.status(400).json({ success: false, error: err });
+        });
+};
+
+
+
+
+const deleteFeedBack = async (req, res)=>{
+  Feedback.findByIdAndRemove(req.params.id).then((feedback)=>{
+      return res.status(200).json({success: true, message: "Feedback deleted", data: feedback});
+  }).catch((err)=>{
+      return res.status(400).json({sucess: false, error: err});
+  });
+};
+
+
   module.exports = {
-   createFeedback
+   createFeedback,
+    getAllFeedback,
+   deleteFeedBack
+
+
 };
   
-  ;
+  
   
