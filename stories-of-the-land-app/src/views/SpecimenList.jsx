6eec99523065f3423dc413199c776ad8e951e3df
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-06-02 13:36:45 -0600

    add search bar

--- stories-of-the-land-app/src/views/SpecimenList.jsx
+++ stories-of-the-land-app/src/views/SpecimenList.jsx
@@ -14,6 +14,7 @@ const backendURL = process.env.REACT_APP_BACKEND_IP || "localhost";
 const SpecimenList = () => {
   const [specimens, setSpecimens] = useState([]);
   const [selectedSpecimen, setSelectedSpecimen] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
   const listRef = useRef(null);
   const location = useLocation();
 
@@ -47,66 +48,81 @@ const SpecimenList = () => {
     setSelectedSpecimen(null);
   };
 
+  // Filter specimens based on the search term
+  const filteredSpecimens = specimens.filter((specimen) =>
+    specimen.plantName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
 
   return (
     <div className={styles.viewContainer}>
-    <NavBar/>
-    <div className="container py-5">
-      <div className="row">
+      <NavBar />
+      <div className="container py-5">
+        <div className="row">
+          <div className="col">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                aria-label="Search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+          </div>
           <div className="col text-end">
             <Link to="/add-specimen" className="btn btn-primary mb-3">
               + Add Specimen
             </Link>
           </div>
-      </div>
-      <div className="row row-cols-1 row-cols-md-3 g-4">
-        {/* Map through the database */}
-        {specimens.map((specimen) => (
-          <div key={specimen._id} className="col" >
+        </div>
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          {filteredSpecimens.map((specimen) => (
+            <div key={specimen._id} className="col">
               <Specimen
                 id={specimen._id}
                 title={specimen.plantName}
                 url={`http://${backendURL}:3001` + specimen.image}
                 onClick={() => handleSpecimenClick(specimen)}
-                // SelectedSpecimenID = {selectedSpecimen._id}
               />
-          </div>
-        ))}
-        <div ref={listRef}/>
-      </div>
-      {/* Modal view */}
-      {selectedSpecimen && (
-        <div
-          className={`${styles.modal} modal fade show`}
-          tabIndex="-1"
-          role="dialog"
-        >
-          <div className="modal-dialog modal-dialog-centered modal-lg" role="document" >
-            <div className="modal-content" style={{backgroundColor: "#edf5e0"}}>
-              <div className="modal-header" style={{backgroundColor: "#edf5e0"}}>
-                <h5 className="modal-title text-center w-100">{selectedSpecimen.plantName}</h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                  onClick={handleCloseModal}
-                />
-              </div>
-              <div className="modal-body d-flex justify-content-center align-items-center flex-column">
-                <img
-                  src={`http://${backendURL}:3001` + selectedSpecimen.image}
-                  alt="the specimen"
-                  className="img-fluid mb-3"
-                  style={{ maxWidth: "100%" }}
-                />
-                <p className="text-center">{selectedSpecimen.story}</p>
+            </div>
+          ))}
+          <div ref={listRef} />
+        </div>
+        {/* Modal view */}
+        {selectedSpecimen && (
+          <div
+            className={`${styles.modal} modal fade show`}
+            tabIndex="-1"
+            role="dialog"
+          >
+            <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+              <div className="modal-content" style={{ backgroundColor: "#edf5e0" }}>
+                <div className="modal-header" style={{ backgroundColor: "#edf5e0" }}>
+                  <h5 className="modal-title text-center w-100">{selectedSpecimen.plantName}</h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={handleCloseModal}
+                  />
+                </div>
+                <div className="modal-body d-flex justify-content-center align-items-center flex-column">
+                  <img
+                    src={`http://${backendURL}:3001` + selectedSpecimen.image}
+                    alt="the specimen"
+                    className="img-fluid mb-3"
+                    style={{ maxWidth: "100%" }}
+                  />
+                  <p className="text-center">{selectedSpecimen.story}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
     </div>
   );
 };
